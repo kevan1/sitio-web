933cb832a42933ec3214f7f125bdefac8fca43a9
Kevan <kevan@192.168.1.2>
2021-05-29 03:31:11 -0300

    add cosas

---
+++ src/Social/Social.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './Social.css';
+
+
+
+
+function Social() {
+  return (
+    <div className="social">
+    Social V2
+    </div>
+  );
+}
+
+export default Social;
